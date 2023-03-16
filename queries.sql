95f225e109d8a686ab9816c3b95d774137c6d70d
AbdifatahYasin1 <iskatag123@gmail.com>
2023-03-16 10:31:05 +0300

    query and update animals table

--- queries.sql
+++ queries.sql
@@ -11,4 +11,15 @@ FROM animals
 WHERE NOT name = 'Gabumon';
 SELECT * from animals WHERE weight_kg between 10.4 and 17.3;
 
+-- Vet clinic database: query and update animals table
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT MAX(escape_attempts) FROM animals WHERE neutered = true;
+SELECT MAX(escape_attempts) FROM animals WHERE neutered = false;
+SELECT MIN(weight_kg), MAX(weight_kg) FROM animals WHERE species = 'digimon';
+SELECT MIN(weight_kg), MAX(weight_kg) FROM animals WHERE species = 'pokemon';
+SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth between '1990-01-01' and '2000-12-31';
+
 
--- schema.sql
+++ schema.sql
@@ -10,3 +10,51 @@ CREATE TABLE animals (
     primary key (id)
 
 );
+
+
+
+ALTER TABLE animals ADD COLUMN species VARCHAR(100);
+
+
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+
+
+
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name like '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species is null;
+COMMIT;
+SELECT * FROM animals;
+
+
+
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+
+
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT savepoint1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO savepoint1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+COMMIT;
+
+
+
+
+
+
+
+
+
